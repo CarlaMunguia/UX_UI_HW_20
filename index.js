ed54870516929d4bb2540475701b05aa6f4be9c1
CarlaMunguia <94584589+CarlaMunguia@users.noreply.github.com>
2022-01-05 21:26:24 -0600

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-console.log("Your index.js file is loaded correctly!");
-
-$("#download").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#myWorkSection").offset().top},
-        'slow');
-});
-
